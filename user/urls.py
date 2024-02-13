8f792928f3f37c1ea45ef2dedc71ebce46cc43c4
Anand VM <anandnambissan2@gmail.com>
2024-02-13 12:36:00 +0530

    Rest FRameorks added

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+#emall\user\urls.py
+
+from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+
+
+
+urlpatterns = [
+    path('',  index, name="index"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
