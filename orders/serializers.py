74aff730c8e2700cbf341086bbdf1fd0939f0bc8
Anand VM <anandnambissan2@gmail.com>
2024-02-15 15:49:20 +0530

    Updated Nested serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -2,8 +2,43 @@
 
 from rest_framework import serializers
 from .models import Orders
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class OrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orders
         fields = "__all__"
+        # Depth - Get all data from Related table
+        # depth = 1
+
+
+        
+# ---------------------------- *NESTED SERIALIZERS*---------------------#
+class UserNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name"]
+
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+
+    customer = serializers.SerializerMethodField()
+    class Meta:
+        model = Orders
+        fields = "__all__"
+
+    def get_customer(self, obj):
+        try:
+            if obj.customer is not None:
+                # serializer  = UserNestedSerializer(obj.customer)
+                # return serializer.data
+                user = User.objects.get(id=obj.customer.id)
+                return user.username
+
+            return None
+        except Exception as ex:
+            return None
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,7 @@
 #emall/orders/views.py
+from django.db.models import Q
+from functools import reduce
+import operator
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
@@ -9,7 +12,7 @@ from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from user.rbac import IsAdmin, IsGetOrAdmin
 from .models import Orders
-from .serializers import OrdersSerializer
+from .serializers import OrdersSerializer,OrderDetailSerializer
 # Create your views here.
 
 def index(request):
@@ -33,6 +36,7 @@ class OrdersListCreateView(ListCreateAPIView):
             filter_field = self.request.query_params.get('filter_field')  # Get the dynamic filter field
             filter_value = self.request.query_params.get('filter_value')  # Get the filter value
 
+
             if user.user_scope in ("MANAGER", "ADMIN"):
                 queryset = queryset # No changes in query set for this condition
             else:
@@ -45,6 +49,7 @@ class OrdersListCreateView(ListCreateAPIView):
             if filter_field and filter_value:
                 filter_args = {filter_field: filter_value}
                 queryset = queryset.filter(**filter_args)
+
 
             # Accessing pagination class and generating paginated queryset
             page = self.paginate_queryset(queryset)
@@ -72,4 +77,4 @@ class OrdersListCreateView(ListCreateAPIView):
 class OrdersDetailsAPIView(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsGetOrAdmin,)
     queryset = Orders.objects.filter(is_deleted=False).order_by('-id')
-    serializer_class = OrdersSerializer
+    serializer_class = OrderDetailSerializer
